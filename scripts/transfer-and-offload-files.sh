e124c09ecf4c67b6ed13822378bb51e3b9bd3760
Cindy Liang <celiang@ucsc.edu>
2026-04-25 02:07:36 +0000

    fix another typo

--- scripts/transfer-and-offload-files.sh
+++ scripts/transfer-and-offload-files.sh
@@ -11,7 +11,7 @@
 # bash scripts/transfer-and-offload-files.sh target checksums shiba
 
 # Usage example for offloading files after transfer
-# bash scripts/transfer-and-offload-files.sh target checksums shiba [timestamped checksum filename.txt]
+# bash scripts/transfer-and-offload-files.sh target offload shiba [timestamped checksum filename.txt]
 
 # cause nonzero exit status and undefined variables to stop the script
 set -euo pipefail

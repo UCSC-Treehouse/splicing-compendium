a740bb8711187f083c8ea7244609f7100ee38082
cindyeliang <celiang@ucsc.edu>
2026-05-21 17:00:10 +0000

    convert NAs from unshared junctions to 0

--- scripts/03-merge_separate_junctions.R
+++ scripts/03-merge_separate_junctions.R
@@ -38,5 +38,8 @@ merged_junctions <- purrr::map(junction_paths, \(file) {
     # the resulting table separates junction counts from each sample by columns with the sample ID
     purrr::reduce(\(x, y) dplyr::full_join(x, y, by = c("chr", "start", "end", "ID")))
 
+# convert NAs from junctions unshared between samples to 0
+merged_junctions[is.na(merged_junctions)] <- 0
+
 ## Save merged junction counts as output
 readr::write_tsv(merged_junctions, opt$output)

efaf68a296a64ff6b665a9d4496d9ad7be70a1dc
Cindy Liang <67753617+cindyeliang@users.noreply.github.com>
2026-07-16 08:32:14 -0700

    Update scripts/03-merge_separate_junctions.R
    Co-authored-by: Joshua Shapiro <josh.shapiro@ccdatalab.org>

--- scripts/03-merge_separate_junctions.R
+++ scripts/03-merge_separate_junctions.R
@@ -59,7 +59,7 @@ merged_junctions <- purrr::map(junction_paths, \(file) {
   # merge junctions tables from multiple samples
   # the resulting table separates junction counts from each sample by columns with the sample ID
   purrr::reduce(
-    \(x, y) dplyr::full_join(
+    \(x, y) duckplyr::full_join(
       x, y,
       by = c("chr", "start", "end", "ID")
     )

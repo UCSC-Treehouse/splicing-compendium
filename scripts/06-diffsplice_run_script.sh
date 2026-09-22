200ff63a1716a4c066a7b373f1e8e83b3075ce32
cindyeliang <celiang@ucsc.edu>
2026-09-21 20:00:16 -0700

    must call on scripts in scripts dir

--- scripts/06-diffsplice_run_script.sh
+++ scripts/06-diffsplice_run_script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # shell script for running diffsplice script through slurm
+# usage: sbatch scripts/06-diffsplice_run_script.sh
 
 #SBATCH --mem=20gb
 #SBATCH --job-name=diffsplice_compendium
@@ -7,10 +8,10 @@
 #SBATCH --account=standard
 #SBATCH --mail-user=celiang@ucsc.edu
 #SBATCH --mail-type=FAIL,END
-#SBATCH --output <my_job-%j.out>
+#SBATCH --output logs/diffsplice_%j.txt
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 
 # run the rscript
-rscript 06-diffsplice_calculation.R --reference_group "Whole Blood" --query_group AML --metadata_file combined_compendium_metadata.tsv --in_psi cleaned_psi_matrix.txt --out_file test_diffsplice.tsv
+Rscript scripts/06-diffsplice_calculation.R --reference_group "Whole Blood" --query_group AML --metadata_file combined_compendium_metadata.tsv --in_psi cleaned_psi_matrix.txt --out_file test_diffsplice.tsv
